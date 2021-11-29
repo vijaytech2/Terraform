a18d42a4993c7ee17f80c9ad56fceb38e80a0c07
vijaytech2 <vijayitchn@gmail.com>
2021-11-29 21:40:57 +0530

    added ec2 template

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_key_pair" "tf-key" {
+  key_name   = "tf-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
+resource "aws_instance" "my-first-instance" {
+  ami = "ami-082b5a644766e0e6f"
+  instance_type = "t2.micro"
+  key_name = "tf-key"
+}
+resource "aws_s3_bucket" "my-terra-bkt" {
+  bucket = "bkt-terraform"
+  acl = "private"
+}
