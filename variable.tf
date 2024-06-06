8ac4cfc588628b3a4188abf7a37ae0c121ff4f12
vijaytech2 <vijayitchn@gmail.com>
2024-06-06 14:27:10 +0530

    updated the variable file

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "linuxami" {
+    default = {
+	 us-east-1 = "ami-00beae93a2d981137"
+	 }
+}
+
+variable "vpc_custom" {
+  default = "192.168.0.0/16"
+  description = "Create a custome VPC"
+}
+variable "subnet-public-AZ-CIDR" {
+  default = "192.168.5.0/24"
+  description = "Create a public subnet"
+}
+variable "subnet-private-AZ-CIDR" {
+  default = "192.168.6.0/24"
+  description = "Create a private subnet"
+}
+variable "key_name" {
+  default = "demowithvenkat"
+  description = "allow ssh using this key"
+}
+ 
+    
